05639d05bbc0660ed224338180a8cf62ecfcaed9
Jonah Nichols <nicholskids.jonah@gmail.com>
2018-12-04 15:14:56 -0800

    Set up configurations

--- main.sh
+++ main.sh
@@ -2,3 +2,10 @@ sudo apt update
 sudo apt upgrade
 sudo ufw enable
 
+udo apt-get remove pure-ftpd
+sudo apt-get install bum
+bum
+
+sudo cp sshd_config /etc/sshd_config
+sudo cp lightdm.conf /etc/lightdm
+
